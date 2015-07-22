cece1f9abcf574a188166a42e36b661ffe0296ba
Denis Tuchin <denistuchin@gmail.com>
2015-07-22 19:26:16 +0300

    LoginPage

--- src/test/java/system/pageobjects/LoginPage.java
+++ src/test/java/system/pageobjects/LoginPage.java
@@ -1,7 +1,49 @@
 package system.pageobjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Created by Денис on 22.07.2015.
  */
 public class LoginPage {
+    private WebDriver driver;
+
+    @FindBy(name = "name")
+    private WebElement userNameField;
+
+    @FindBy(name = "password")
+    private WebElement passwordField;
+
+    @FindBy(css = "input[type=\"submit\"]")
+    private WebElement submitButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getUserNameField() {
+        return userNameField;
+    }
+
+    public void setUserNameField(WebElement userNameField) {
+        this.userNameField = userNameField;
+    }
+
+    public WebElement getPasswordField() {
+        return passwordField;
+    }
+
+    public void setPasswordField(WebElement passwordField) {
+        this.passwordField = passwordField;
+    }
+
+    public WebElement getSubmitButton() {
+        return submitButton;
+    }
+
+    public void setSubmitButton(WebElement submitButton) {
+        this.submitButton = submitButton;
+    }
 }
